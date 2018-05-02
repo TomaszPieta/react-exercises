efd58566fdcd47322662f1717cda67394f86dc2a
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-05-02 09:35:26 +0200

    commit

---
+++ state-switch-filter/script.js
@@ -0,0 +1,16 @@
+﻿let prevScrollTop;
+let scrolling;
+function scrolled(){
+  let scrollTop = window.scrollY;
+  let nav = document.querySelector('nav');
+  if (scrollTop > prevScrollTop){
+    nav.classList.add('upper');
+  } else {
+    nav.classList.remove('upper')  }
+  prevScrollTop = scrollTop; }
+document.addEventListener('scroll', function(){
+  scrolling = true}, false);
+setInterval(function(){
+  if (scrolling){
+    scrolled();
+    scrolling = false;  }}, 100);
