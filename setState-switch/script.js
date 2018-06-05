3fdb77f491a0cf4313f309410c65fb31d69a7a69
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-06-05 16:52:44 +0200

    commit

---
+++ setState-switch/script.js
@@ -0,0 +1,30 @@
+﻿class Portfolio extends Component{
+  state = {
+    array: [...Data]
+  }
+  filterHandler = (event) => {
+    let category = event.target.getAttribute('category');
+    switch (category) {
+      case 'first': {
+        this.setState({
+          array: [...Data].filter(
+            (array) => array.category === 'first'
+          )}); break; }
+      case 'second': {
+        this.setState({
+          array: [...Data].filter(
+          (array) => array.category === 'second'
+          )}); break; }
+      case 'third': {
+        this.setState({
+          array: [...Data].filter(
+          (array) => array.category === 'third'
+          )}); break; }
+      default: {
+        this.setState({
+          array: [...Data]
+        })
+      }
+    }
+  }
+}
